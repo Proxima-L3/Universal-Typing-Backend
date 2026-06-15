1c7634c8c77348fc784545abfb340f840cf06c8e
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-06-14 17:18:00 -0700

    Made migration from sqlite3 to postgresql

--- typing_site_backend/settings.py
+++ typing_site_backend/settings.py
@@ -90,8 +90,14 @@ WSGI_APPLICATION = 'typing_site_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'typing_site_db',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
