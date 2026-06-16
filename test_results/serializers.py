19e014cc23c5c4fc21ddd6fa75dd4c39ca511e51
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-06-16 16:34:29 -0700

    Added a custom action for TestResultsViewSet that processes different conditions for returning a serialized test results page leaderboard response. Also made another serializer specifically for returning requested data

--- test_results/serializers.py
+++ test_results/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 # import the test results data model
 from .models import TestResults
 
-# create a serializer class
+# serializer class for converting viewsets responses to and from JSON between it and models.py
 class TestResultsSerializer(serializers.ModelSerializer):
 
     # create a meta class
@@ -38,4 +38,23 @@ class TestResultsSerializer(serializers.ModelSerializer):
                   "test_show_stats",
                   "test_show_timer",
                   "test_show_word_count"
+                  )
+
+# serializer class for converting viewsets responses to and from JSON between it and models.py
+class TestResultsLeaderboardSerializer(serializers.ModelSerializer):
+    
+    # have to declare rank as an "explicit field on the serializer class" otherwise the serializer will try looking for rank in the database and error
+    rank = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = TestResults
+        fields = ("id",
+                  "rank",
+                  "username_tag",
+                  "test_typing_speed_kps",
+                  "test_typing_speed_kph",
+                  "test_typing_speed_cpm",
+                  "test_typing_speed_wpm",
+                  "test_typing_accuracy",
+                  "test_overall_score"
                   )
--- test_results/views.py
+++ test_results/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.db.models import F, Window
+from django.db.models.functions import RowNumber
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import TestResultsSerializer
+from .serializers import TestResultsLeaderboardSerializer
 from .models import TestResults
 
 # note: views are functions or class object methods that take in http requests as parameters and return http responses
@@ -13,10 +18,83 @@ from .models import TestResults
 class TestResultsViewSet(viewsets.ModelViewSet):
 
     # assigning test results translation process (between JSON and django model instances) to serializer class TestResultsSerializer
-    serializer_instance = TestResultsSerializer
+    serializer_class = TestResultsSerializer
 
     # variable populated with ALL test results entry rows of sql table
     queryset = TestResults.objects.all()
+
+    @action(detail=False, methods=['get'])
+    def return_test_results_page_leaderboard(self, request):
+        # save query params filters to vars
+        test_type = request.query_params.get('test_type')
+        specialization_field = request.query_params.get('specialization_field')
+        entry_id = request.query_params.get('entry_id')
+        # use query params to grab all entries that match request test_type and specialization_field
+        filtered_results = self.queryset.filter(
+            test_type=test_type,
+            test_specialization_field=specialization_field
+            )
+        # order returned query results by test_overall_score
+        ordered_results = filtered_results.order_by('-test_overall_score')
+        # assign rank field to each row based on currently ordered queryset (that is ordered by highest to lowest score)
+        ranked_results = ordered_results.annotate(
+            rank=Window(
+                expression=RowNumber(),
+                order_by=F('test_overall_score').desc()
+            )
+        )
+        # grab the user's row and their rank value of their entry row
+        # user_row = ranked_results.filter(id=entry_id)
+        user_rank = ranked_results.filter(id=entry_id).values('rank')[0].get('rank')
+        # grab length of user_results ONCE using len() will trigger a SELECT COUNT(*) sql query every time which is inefficient
+        ranked_results_length = ranked_results.count()
+        # declare serializer to be used to return response data
+        leaderboard_serializer_class = TestResultsLeaderboardSerializer
+
+        # if total entries is at least 51
+        if ranked_results_length >= 51:
+            # if user's rank is not in top 25 and not in bottom 25
+            # if user_rank > 25 and user_rank not in ranked_results.values('rank')[-1:-26:-1].get('rank'):
+            if user_rank > 25 and user_rank <= ranked_results_length - 25:
+                # then grab the 25 rows above user rank and the 25 below and concatenate them into one list of object rows then return that list
+                # above_25rows = ranked_results[(ranked_results[user_rank - 26]):(ranked_results.index(user_row)):1]
+                # above_25rows = ranked_results[(user_rank - 26):(ranked_results.index(user_row)):1]
+                # below_25rows = ranked_results[(ranked_results.index(user_row) + 1):(user_rank + 25):1]
+
+                # test_results_leaderboard_51rows = above_25rows + user_row + below_25rows
+                # test_results_leaderboard_51rows = above_25rows | user_row | below_25rows
+                test_results_leaderboard_51rows = ranked_results[(user_rank - 26):(user_rank + 25):1]
+
+                # serialize the data before returning response
+                serializer_instance = leaderboard_serializer_class(test_results_leaderboard_51rows, many=True)
+                return Response(serializer_instance.data)
+            # else if user's rank is in the bottom 25
+            elif user_rank > ranked_results_length - 25:
+                # then grab the bottom 51 rows and return that list
+                # bottom_51rows = ranked_results[-1:-52:-1]
+                bottom_51rows = ranked_results[(ranked_results_length - 51):ranked_results_length:1]
+                
+                # serialize the data before returning response
+                serializer_instance = leaderboard_serializer_class(bottom_51rows, many=True)
+                return Response(serializer_instance.data)
+            # else if user's rank is in the top 25
+            elif user_rank <= 25:
+                # then grab the top 51 rows and return that list
+                top_51rows = ranked_results[0:51:1]
+
+                # serialize the data before returning response
+                serializer_instance = leaderboard_serializer_class(top_51rows, many=True)
+                return Response(serializer_instance.data)
+            else:
+                return Response({'error': 'Unexpected error: paradox found'}, status=400)
+                # print('Unexpected Error: user_rank not found?')
+        # else total entries is less than 51 currently so return all that is present
+        else:
+            # serialize the data before returning response
+            serializer_instance = leaderboard_serializer_class(ranked_results, many=True)
+            return Response(serializer_instance.data)
+
+        # return 51 row window as a list of object rows with only id, rank, username_tag, test_overall_score, accuracy, wpm, cpm, kps, and kph (serializer will convert to json for me i think?)
 
 
 # Handles public leaderboard get/put/push/post requests
